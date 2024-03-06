252f08cc3a4f4f8b1ca6e561c99e8cd06d74321a
fire833 <kttpsy@gmail.com>
2024-03-06 13:00:56 -0600

    feat: add fire dataset to the package for lab 7

---
+++ R/data.R
@@ -0,0 +1,31 @@
+# Copyright (C) 2024 Kendall Tauser
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+#' @title Fire data set.
+#'
+#' @description  A data set concerning damage from fire
+#'
+#' @details This is a standard data set from the course MATH 4753
+#' taken from the data sets provided by the text book.
+#'
+#' @format A data frame with 15 rows and 2 variables:
+#' \describe{
+#'   \item{DISTANCE}{Distance in miles from fire station}
+#'   \item{DAMAGE}{Thouands of dollars}
+#' }
+#'
+#'@source \url{https://www.routledge.com/Statistics-for-Engineering-and-the-Sciences-Sixth-Edition/Mendenhall-Sincich/p/book/9781498728850} # nolint
+"fire"
