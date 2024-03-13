253a590c4a9dc94582b4c83810994d6b1b29d6c4
fire833 <kttpsy@gmail.com>
2024-03-13 09:45:04 -0500

    feat: add additional utils including chebyshev, bayes testing, etc.

--- R/utils.R
+++ R/utils.R
@@ -23,7 +23,7 @@
 #' @examples
 #' z <- get_z(c(1, 2, 3, 4, 5))
 get_z <- function(vector) {
-  (vector - mean(vector)) / stats::sd(vector)
+  return((vector - mean(vector)) / stats::sd(vector))
 }
 
 #' @param vector provide the vector you wish to get the variance from
@@ -35,7 +35,7 @@ get_z <- function(vector) {
 #' @examples
 #' s_squared <- get_variance(c(1, 2, 3, 4, 5))
 get_variance <- function(vector) {
-  sum((vector - mean(vector))^2 / length(vector) - 1)
+  return(sum((vector - mean(vector))^2 / length(vector) - 1))
 }
 
 #' @param y provide the vector of actual values you are wishing
@@ -51,7 +51,7 @@ get_variance <- function(vector) {
 #' @examples
 #' tss <- get_tss(c(1, 2, 3, 4, 5), c(2, 3, 4, 5, 6))
 get_tss <- function(y, yhat) {
-  sum((y - mean(y))^2)
+  return(sum((y - mean(y))^2))
 }
 
 #' @param y provide the vector of actual values you are wishing
@@ -67,7 +67,7 @@ get_tss <- function(y, yhat) {
 #' @examples
 #' mss <- get_mss(c(1, 2, 3, 4, 5), c(2, 3, 4, 5, 6))
 get_mss <- function(y, yhat) {
-  sum((y - yhat)^2)
+  return(sum((y - yhat)^2))
 }
 
 #' @param y provide the vector of actual values you are wishing
@@ -83,5 +83,55 @@ get_mss <- function(y, yhat) {
 #' @examples
 #' rss <- get_rss(c(1, 2, 3, 4, 5), c(2, 3, 4, 5, 6))
 get_rss <- function(y, yhat) {
-  sum((yhat - mean(y))^2)
+  return(sum((yhat - mean(y))^2))
+}
+
+#' @param k provide the number of standard deviations from the
+#' mean you are hoping to capture.
+#'
+#' @title Get the minimum percentage of observations within k
+#' standard deviations of the mean for any given sample.
+#' @return The percentage of sample within k standard deviations
+#' of the mean.
+#' @export
+#'
+#' @examples
+#' pct <- get_chebyshev(2)
+#' pct <- get_chebyshev(4)
+get_chebyshev <- function(k) {
+  return(1 - 1 / (k^2))
+}
+
+#' @param x The x param to utilize in your table.
+#' @param y The y param to utilize in your table.
+#'
+#' @title Create a table with marginals given two variables.
+#' @return The table printed to stdout.
+#' @export
+#'
+#' @examples
+#' get_table_with_margins(c(1,2,3,4), c(4,5,6,7))
+get_table_with_margins <- function(x, y) {
+  stats::addmargins(table(x, y))
+}
+
+#' @param u The probability someone is a user
+#' @param tu The probability someone tests positive
+#' given they are a user. More specifically, what is the
+#' probability for a true positive.
+#' @param tubar The probability somenoe tests positive given
+#' that they aren't a user. More specifically, what is the
+#' probability someone gets a false positive when they aren't
+#' a user.
+#'
+#' @title Run Baye's testing theorem on given probabilities.
+#' @return The probability someone is a user given they test
+#' positive.
+#' @export
+#'
+#' @examples
+#' pct <- get_bayes_testing(3, 95, 30)
+get_bayes_testing <- function(u, tu, tubar) {
+  ubar <- 1 - u
+  return((u * tu) / ((u * tu) + (ubar * tubar)))
 }
